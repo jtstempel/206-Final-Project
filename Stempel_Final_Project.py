b2abe42a4836f9eae573b19a1352eb0aff778a01
Joseph Stempel <jstempel@umich.edu>
2017-12-07 14:15:17 -0500

    Thursday Afternoon Commit

--- Stempel_Final_Project.py
+++ Stempel_Final_Project.py
@@ -1,55 +1,60 @@
 ## Joseph Stempel
 
-## import statements?
-
+import facebook
 import unittest
 import json
 import sqlite3
-import webbrowser
-from requests_oauthlib import OAuth2Session
-from requests_oauthlib.compliance_fixes import facebook_compliance_fix
+import requests
+## from pprint import pprint
 
-
-## Facebook API
+## import webbrowser
+## import itertools
+## import collections
 
 my_app_id = '2497254837080155'
 my_app_secret = '53b9b633590e8bb438abb4b74bf5f153'
+my_access_token = 'EAAjfPXN6UFsBAB1wthsVg58esuEtrR5nZARGVmZCUSZCMdgEmIFA6W8IdYd4XGwp10XcSh68jqwGXVA83uE0jOW8rhHVl0cYn0HJfeolbYE3dWbdumifrNJbDY0eTPejMZBbqxZB05XsGkoRfRkxqYnLj1QhfF0yR6yWGG9JtK20LEhvQe7X8J3nc5ABktnD16laJDk3v9gZDZD'
+## user_id = '1780949455269404'
 
-## Caching Setup
-
-CACHE_FNAME = 'Stempel_Final_Project_Cache.json'
-
-try:
-    cache_file = open(CACHE_FNAME,'r')		
-    cache_contents = cache_file.read()
-    cache_file.close()
-    CACHE_DICTION = json.loads(cache_contents)
-except:
-    CACHE_DICTION = {}
-
-def myFacebookAuthorization():
-	pass
-
-def getUserPosts(my_user):
-	if my_user in CACHE_DICTION:
-		print ('Using Cache...')
-		my_info = CACHE_DICTION[my_user]
-		return my_info
-	else:
-		print ('Fetching data...')
-		my_info = ...
+my_graph = facebook.GraphAPI(access_token = my_access_token, version = '2.1')
+posts_on_my_timeline = my_graph.request('/me/feed')
+print (posts_on_my_timeline)
 
 
-## Access exactly 100 interactions
 
-## Find the days that each of these interactions took place (find respective dates and times of posts)
 
-## Find content of each post
+##CACHE_FNAME = 'Stempel_Final_Project_Cache.json'
 
-## Find number of likes, number of shares?
+##try:
+##    cache_file = open(CACHE_FNAME,'r')		
+##    cache_contents = cache_file.read()
+##    cache_file.close()
+##    CACHE_DICTION = json.loads(cache_contents)
+##except:
+##    CACHE_DICTION = {}
 
-## Potentially find comments on posts
 
-## Write the data to a database
+##def get_user_posts(my_user):
 
-## Create a report
+##	if my_user in CACHE_DICTION:
+##		print ('Using Cache...')
+##		my_info = CACHE_DICTION[my_user]
+##		return my_info
+##	else:
+##		print ('Fetching data...')
+##		my_info = ...
+##		CACHE_DICTION[my_user] = my_info
+
+##		writing_file = open(CACHE_DICTION, 'w')
+##		writing_file.write(json.dumps(CACHE_DICTION))
+##		writing_file.close()
+
+##	return my_info
+
+
+
+
+
+
+
+
