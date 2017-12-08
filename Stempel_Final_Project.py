942f6ae1f7bd6b7ea0c3d3cd0e96932cd1ff7070
Joseph Stempel <jstempel@umich.edu>
2017-12-08 13:24:50 -0500

    Friday Afternoon Commit

--- Stempel_Final_Project.py
+++ Stempel_Final_Project.py
@@ -1,37 +1,37 @@
 ## Joseph Stempel
 
+## Import Statements
+
 import facebook
 import unittest
 import json
 import sqlite3
 import requests
-## from pprint import pprint
+import Facebook_Info
+from pprint import pprint
 
 ## import webbrowser
 ## import itertools
 ## import collections
 
-my_app_id = '2497254837080155'
-my_app_secret = '53b9b633590e8bb438abb4b74bf5f153'
-my_access_token = 'EAAjfPXN6UFsBAB1wthsVg58esuEtrR5nZARGVmZCUSZCMdgEmIFA6W8IdYd4XGwp10XcSh68jqwGXVA83uE0jOW8rhHVl0cYn0HJfeolbYE3dWbdumifrNJbDY0eTPejMZBbqxZB05XsGkoRfRkxqYnLj1QhfF0yR6yWGG9JtK20LEhvQe7X8J3nc5ABktnD16laJDk3v9gZDZD'
-## user_id = '1780949455269404'
+my_app_id = Facebook_Info.my_app_id
+my_app_secret = Facebook_Info.my_app_secret
+my_access_token = Facebook_Info.my_access_token
 
 my_graph = facebook.GraphAPI(access_token = my_access_token, version = '2.1')
 posts_on_my_timeline = my_graph.request('/me/feed')
-print (posts_on_my_timeline)
+pprint (posts_on_my_timeline)
 
 
+CACHE_FNAME = 'Stempel_Final_Project_Cache.json'
 
-
-##CACHE_FNAME = 'Stempel_Final_Project_Cache.json'
-
-##try:
-##    cache_file = open(CACHE_FNAME,'r')		
-##    cache_contents = cache_file.read()
-##    cache_file.close()
-##    CACHE_DICTION = json.loads(cache_contents)
-##except:
-##    CACHE_DICTION = {}
+try:
+	cache_file = open(CACHE_FNAME,'r')		
+	cache_contents = cache_file.read()
+	cache_file.close()
+	CACHE_DICTION = json.loads(cache_contents)
+except:
+	CACHE_DICTION = {}
 
 
 ##def get_user_posts(my_user):
